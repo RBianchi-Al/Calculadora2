70fb44387a71583bb3cb7ca371c18ec51d507f07
RBianchi-Al <renatalebianchi@gmail.com>
2020-04-02 20:55:21 -0300

    versao 2

---
+++ src/calculadora.service.js
@@ -0,0 +1,60 @@
+function CalculadoraService() {
+    const SOMA = '+';
+    const SUB = '-';
+    const DIV = '/';
+    const MULT = '*';
+    function calcular(numero1, numero2, operacao) {
+        let resultado;
+        switch (operacao) {
+            case SOMA:
+                resultado = numero1 + numero2
+                break;
+            case SUB:
+                resultado = numero1 - numero2
+                break;
+            case DIV:
+                resultado = numero1 / numero2
+                break;
+            case MULT:
+                resultado = numero1 * numero2
+                break;
+            default:
+                resultado = 0;
+        }
+
+        return resultado
+
+    }
+
+    function concatenarNumero(numAtual, numConcat){
+
+     if (numAtual === '0' || numAtual === null){
+      numAtual = '';
+     }
+   
+
+     if (numConcat === '.' && numAtual === ''){
+       return'0.';
+     }
+
+
+     if (numConcat === '.' && numAtual.indexOf('.') > -1){
+       return numAtual;
+     }
+
+
+    return numAtual + numConcat;
+    }
+
+
+    return [
+        calcular,
+        concatenarNumero,
+        SOMA, 
+        SUB,
+        DIV,
+        MULT
+    ];
+
+}
+export default CalculadoraService;
