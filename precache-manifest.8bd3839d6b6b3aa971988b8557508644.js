3a37fb6a4e7f5daeaf7ded6065179229cc73927e
RBianchi-Al <renatalebianchi@gmail.com>
2020-04-03 18:51:31 -0300

    Updates

---
+++ precache-manifest.8bd3839d6b6b3aa971988b8557508644.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068cadb4cf0794b9b3b60127f9fe0e67",
+    "url": "/Conversor-de-moedas/index.html"
+  },
+  {
+    "revision": "2d8e34c9dca682d78a4f",
+    "url": "/Conversor-de-moedas/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a8a016e20a6e211d7323",
+    "url": "/Conversor-de-moedas/static/css/main.81ed1671.chunk.css"
+  },
+  {
+    "revision": "2d8e34c9dca682d78a4f",
+    "url": "/Conversor-de-moedas/static/js/2.f1b1d6fc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Conversor-de-moedas/static/js/2.f1b1d6fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a016e20a6e211d7323",
+    "url": "/Conversor-de-moedas/static/js/main.fe9c040e.chunk.js"
+  },
+  {
+    "revision": "b656d78c73f1bac42c83",
+    "url": "/Conversor-de-moedas/static/js/runtime-main.165bcc92.js"
+  }
+]);
